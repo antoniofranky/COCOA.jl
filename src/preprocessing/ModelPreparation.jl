04277947503fe534ef338e98d67c22bbbb7c2851
antoniofranky <antonschaf@posteo.de>
2025-06-30 15:46:56 +0200

    changing rev split

--- src/preprocessing/ModelPreparation.jl
+++ src/preprocessing/ModelPreparation.jl
@@ -32,7 +32,7 @@ function prepare_model_for_concordance(model::AbstractFBCModel;
     optimizer=HiGHS.Optimizer,
     flux_tolerance::Float64=1e-9,
     remove_blocked::Bool=true,
-    split_reversible::Bool=flase, # Already handled through COBREXA constraints
+    split_reversible::Bool=false, # Already handled through COBREXA constraints
     remove_zero_rows::Bool=true,
     remove_zero_cols::Bool=true,
     workers=Distributed.workers(),
